fcfa00d768880a2fca1f7d8fb3ec1e5e94f24edf
Jdsatashi <vdt1073@gmail.com>
2023-08-01 14:16:02 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,44 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+	<header-top/>
+	<img alt="Vue logo" src="./assets/logo.png">
+	<HelloWorld ref="hello" msg="Hello there"/>
+	<base-modal title="The title of app component" body="This is the content of modal from app"
+				theme="test"></base-modal>
+	<button @click="onChangeTest">Click</button>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import HeaderComponent from "@/components/HeaderComp.vue";
+// eslint-disable-next-line no-unused-vars
+import MyModal from "@/components/MyModal.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+	name: 'App',
+	components: {
+		HeaderTop: HeaderComponent,
+		HelloWorld,
+		BaseModal: MyModal,
+	},
+	data() {
+		return {}
+	},
+	methods: {
+		onChangeTest() {
+			console.log('This is a test ' + Math.random())
+			console.log(this)
+			this.$refs.hello.test()
+		}
+	}
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
 }
 </style>
---
+++ src/components/HeaderComp.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="header-top">
+    <h1>Header component</h1>
+    <a></a>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+.header-top {
+  color: red;
+  font-weight: bold;
+  font-size: 30px;
+}
+</style>
